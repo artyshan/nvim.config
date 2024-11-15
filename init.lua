4eee47d49231aeb2bcc6027ff4361993535790f7
artyshan <artyshan@gmail.com>
2024-11-15 20:09:57 +0100

    added some custom keymaps

--- init.lua
+++ init.lua
@@ -190,6 +190,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- My custom keymaps
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('n', ':', ';')
+vim.keymap.set('i', 'kj', '<Esc>')
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
